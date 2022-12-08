4ec72b77d6e57f2abd50345a6e7e46fadc29b645
Lourenco <lourencodematos@gmail.com>
2022-12-08 14:47:03 +0000

    day6

--- day6.py
+++ day6.py
@@ -1,12 +1,30 @@
-file = open("input", "r")
-f = file.read()
-marker = 0
-sequence = []
-for letter in f:
-    marker += 1
-    if letter in sequence:
-        sequence = []
-    sequence.append(letter)
-    if len(sequence) == 4:
-        print(marker)
-        break
+f = open("input", "r")
+file = f.read()
+
+
+def part1():
+    marker = 0
+    sequence = []
+    for letter in file:
+        marker += 1
+        if letter in sequence:
+            sequence = []
+        sequence.append(letter)
+        if len(sequence) == 4:
+            return marker
+
+
+def part2():
+    marker = 0
+    sequence = []
+    for letter in file:
+        marker += 1
+        if letter in sequence:
+            sequence = []
+            continue
+        sequence.append(letter)
+        if len(sequence) == 14:
+            return marker
+
+
+print(part2())
