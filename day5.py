ba70a1e1176851e5a2b88fe96217bd6bf3e4124a
Lourencom <lourencodematos@gmail.com>
2022-12-08 14:10:14 +0000

    part2

--- day5.py
+++ day5.py
@@ -1,36 +1,36 @@
-def part1():
-    stacks = []
-    for i in range(9):
-        stacks.append([])
-    f = open("input", "r")
-    lines = f.readlines()
-    currstack = 0
-    for col in range(1, 3*9 + 8, 4):
-        for row in range(8):
-            if lines[row][col] == " ":
-                continue
-            stacks[currstack].append(lines[row][col])
-        currstack += 1
+part1 = False
+stacks = []
+for i in range(9):
+    stacks.append([])
+f = open("input", "r")
+lines = f.readlines()
+currstack = 0
+for col in range(1, 3*9 + 8, 4):
+    for row in range(8):
+        if lines[row][col] == " ":
+            continue
+        stacks[currstack].append(lines[row][col])
+    currstack += 1
 
-    for i in range(9):
-        stacks[i].reverse()
+for i in range(9):
+    stacks[i].reverse()
 
-    for i in range(10, len(lines)):
-        line = lines[i].split()
-        quantity = int(line[1])
-        stackFrom = stacks[int(line[3]) - 1]
-        stackTo = stacks[int(line[5]) - 1]
-        toMove = stackFrom[-quantity:]
-        stackFrom = stackFrom[:-quantity]
-        print(toMove)
+for i in range(10, len(lines)):
+    line = lines[i].split()
+    quantity = int(line[1])
+    stackFrom = stacks[int(line[3]) - 1]
+    stackTo = stacks[int(line[5]) - 1]
+    toMove = stackFrom[-quantity:]
+    stackFrom = stackFrom[:-quantity]
+    print(toMove)
+    if part1:
         toMove.reverse()
-        for elem in toMove:
-            stackTo.append(elem)
-        stacks[int(line[3]) - 1] = stackFrom
-        stacks[int(line[5]) - 1] = stackTo
+    for elem in toMove:
+        stackTo.append(elem)
+    stacks[int(line[3]) - 1] = stackFrom
+    stacks[int(line[5]) - 1] = stackTo
 
-    out = ""
-    for i in stacks:
-        out += i[len(i) - 1]
-    print(out)
-
+out = ""
+for i in stacks:
+    out += i[len(i) - 1]
+print(out)
